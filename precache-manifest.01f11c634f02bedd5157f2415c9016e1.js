cef113e6fc078b1c23d396be3d74ef2abbd11313
sandeepPainkra <samdeeppainkra@gmail.com>
2020-12-18 10:58:13 +0530

    Updates

---
+++ precache-manifest.01f11c634f02bedd5157f2415c9016e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6656fa3a1dd23323440a301c3b2bfb77",
+    "url": "/review/index.html"
+  },
+  {
+    "revision": "fb0ce738e215e04966e4",
+    "url": "/review/static/css/main.93df1f20.chunk.css"
+  },
+  {
+    "revision": "03577dc0db4cb31d40df",
+    "url": "/review/static/js/2.af9422dc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/review/static/js/2.af9422dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb0ce738e215e04966e4",
+    "url": "/review/static/js/main.9db26b02.chunk.js"
+  },
+  {
+    "revision": "1c4a6604cb0ad9108044",
+    "url": "/review/static/js/runtime-main.5e019647.js"
+  }
+]);
